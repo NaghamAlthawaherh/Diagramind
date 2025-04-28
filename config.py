3f0cd685689146f06dd3d64452162c42d3798fd7
NaghamAlthawaherh <naghamdwaherh@gmail.com>
2025-04-28 16:15:42 +0300

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-# config.py
-class Config:
-    SQLALCHEMY_DATABASE_URI = 'mssql+pyodbc://@DESKTOP-51BRH30/DiagraMind?driver=ODBC+Driver+17+for+SQL+Server&trusted_connection=yes'
-    # db = SQLAlchemy(app)
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    
